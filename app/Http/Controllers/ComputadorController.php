3bc03ab808b9813598761d7c229405437522bac6
Jamil Gomes <jamil.lannister23@gmail.com>
2018-09-07 15:52:14 -0300

    Add files via upload

---
+++ app/Http/Controllers/ComputadorController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Computador;
+use App\Laboratorio;
+use App\Http\Requests\ComputadorFormRequest;
+
+class ComputadorController extends Controller
+{
+
+  private $computador;
+
+
+      public function __construct(Computador $comp)
+      {
+        $this->computador = $comp;
+      }
+
+    public function index()
+    {
+      $title = "Listagem dos Computadores";
+      $comps = $this->computador->paginate(5); // select * from computadores
+
+      return view('indexComp', compact('comps','title')); //chama a view que vai listar
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      $title = "Cadastro de Computadores";
+      $labs = Laboratorio::all();
+
+
+      return view('create-update-comp',compact('title','labs'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ComputadorFormRequest $request)
+    {
+      $dataform = $request->all();
+
+      $dataform['ativo'] = (!isset($dataform['ativo'])) ? 0 : 1;
+
+       //dd($dataform);
+      $insert = Computador::create($dataform);
+
+      if ($insert) {
+        return redirect()->route('computadores.index');
+      } else {
+        return redirect()->back();
+      }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $comp = $this->computador->find($id);
+      $title = "Visualizar Computador";
+
+      return view('showComp',compact('comp', 'title'));
+    }
+
+
+    public function edit($id)
+    {
+      $comp = $this->computador->find($id);
+      $title = "Atualizar Computador: {$comp->nome}";
+      $labs = Laboratorio::all();
+      return view('create-update-comp',compact('title','comp','labs'));
+    }
+
+
+    public function update(ComputadorFormRequest $request, $id)
+    {
+      $dataform = $request->all();
+      $comp = $this->computador->find($id);
+
+      $update = $comp->update($dataform);
+
+      if($update)
+      {
+        return redirect()->route('computadores.index');
+      }else{
+        return redirect()->route('computadores.edit',$id)->with(['errors'=>'Falha ao Editar!']);
+      }
+    }
+
+
+    public function destroy($id)
+    {
+      $comp = $this->computador->find($id);
+      $delete = $comp->delete();
+
+        if ($delete) {
+          return redirect()->route('computadores.index');
+        }else{
+          return redirect()->route('computadores.show',$id)->with(['errors'=>'Falha ao deletar']);
+        }
+    }
+}
---
+++ app/Http/Controllers/LaboratorioController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Laboratorio;
+use App\Http\Requests\LaboratorioFormRequest;
+class LaboratorioController extends Controller
+{
+   private $laboratorio;
+
+
+   public function __construct(Laboratorio $lab)
+   {
+     $this->laboratorio = $lab;
+   }
+
+    public function index()
+    {
+      $title = "Listagem dos Laboratórios";
+      $labs = $this->laboratorio->paginate(5); // select * from laboratorios
+      return view('indexLab', compact('labs','title')); //chama a view que vai listar
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      $title = "Cadastro de Laboratórios";
+      return view('create-update-lab',compact('title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(LaboratorioFormRequest $request)
+    {
+      //pega tudo no formulario
+      $dataform = $request->all();
+
+      $insert = $this->laboratorio->create($dataform);
+
+      if ($insert) {
+        return redirect()->route('laboratorios.index');
+      } else {
+        return redirect()->back();
+      }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $lab = $this->laboratorio->find($id);
+      $title = "Visualizar Laboratório";
+
+      return view('showLab',compact('lab', 'title'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $lab = $this->laboratorio->find($id);
+      $title = "Atualizar Laboratório: {$lab->nome}";
+
+    return view('create-versao2',compact('title','lab'));
+    }
+
+
+    public function update(LaboratorioFormRequest $request, $id)
+    {
+      $dataform = $request->all();
+      $lab = $this->laboratorio->find($id);
+
+      $update = $lab->update($dataform);
+
+      if($update)
+      {
+        return redirect()->route('laboratorios.index');
+      }else{
+        return redirect()->route('laboratorios.edit',$id)->with(['errors'=>'Falha ao Editar!']);
+      }
+    }
+
+
+  
+    public function destroy($id)
+    {
+      $lab = $this->laboratorio->find($id);
+      $delete = $lab->delete();
+
+        if ($delete) {
+          return redirect()->route('laboratorios.index');
+        }else{
+          return redirect()->route('laboratorios.show',$id)->with(['errors'=>'Falha ao deletar']);
+        }
+    }
+}
