9115218aeac907810fe440b7d6066cc2c5da4f21
Jamil Gomes <jamil.lannister23@gmail.com>
2018-09-07 16:01:50 -0300

    Add files via upload

---
+++ resources/views/layouts/template1.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="stylesheet" href=" {{url( 'css/style.css' )}}">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="container">
+    @yield('content')
+   </div>
+   
+  </body>
+</html>
